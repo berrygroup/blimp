ef174c041aa7eb7434f1fbf6c31af3f2a2946cf6
Scott Berry <z3532965@katana1.gen>
2022-06-29 22:33:39 +1000

    initial repository setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='blimp',
+    version='0.1.0',
+    author='berrygroup',
+    author_email='scottdberry@gmail.com',
+    packages=['blimp', 'blimp.processing','blimp.preprocessing'],
+    scripts=[],
+    url='https://github.com/berrygroup/blimp',
+    license='LICENSE.txt',
+    description='Berry lab main image processing utilities',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
