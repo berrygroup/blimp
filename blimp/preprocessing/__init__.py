5041564d5734db62417d0fb776cebda3cdfb4425
scottberry <scottdberry@gmail.com>
2023-02-08 02:07:02 +1100

    object-oriented illumination correction

--- blimp/preprocessing/__init__.py
+++ blimp/preprocessing/__init__.py
@@ -10,10 +10,7 @@ from .nd2_to_ome_tiff import nd2_to_ome_tiff, convert_individual_nd2_to_ome_tiff
 from .convert_operetta import convert_operetta, find_images_dirs
 from .nd2_parse_metadata import nd2_extract_metadata_and_save
 from .operetta_to_ome_tiff import operetta_to_ome_tiff
-from .illumination_correction import (
-    fit_illumination_correction,
-    apply_illumination_correction,
-)
+from .illumination_correction import correct_illumination, IlluminationCorrection
 from .operetta_parse_metadata import (
     get_image_metadata,
     get_plate_metadata,
--- blimp/preprocessing/illumination_correction.py
+++ blimp/preprocessing/illumination_correction.py
@@ -1,61 +1,204 @@
 from typing import List, Union, Literal
+from pathlib import Path
+import pickle
+import logging
 
-from basicpy import BaSiC as BasicIlluminationCorrection
 from aicsimageio import AICSImage
 from aicsimageio.transforms import transpose_to_dims
 import numpy as np
+import basicpy
+import dask.array as da
 
 from blimp.utils import (
+    equal_dims,
     average_images,
     concatenate_images,
     check_uniform_dimension_sizes,
 )
 
+logger = logging.getLogger(__name__)
 
-def fit_illumination_correction(
-    reference_images: List[AICSImage], timelapse: bool = False, **kwargs
-) -> List[BasicIlluminationCorrection]:
 
-    try:
-        if not check_uniform_dimension_sizes(reference_images):
-            raise ValueError(
-                "Check input. One or more of the ``reference_images`` has non-uniform or incorrect dimensionality"
+class IlluminationCorrection:
+    def __init__(
+        self,
+        reference_images: Union[List[AICSImage], List[str], List[Path], None] = None,
+        timelapse: Union[bool, Literal["multiplicative"], Literal["additive"]] = None,
+        from_file: Union[str, Path] = None,
+        **kwargs,
+    ):
+
+        self._timelapse = timelapse
+        self._dims = None
+        self._file_path = None
+        self._file_name = None
+        self._correctors = None
+
+        # 1. Initialise using reference images and set correctors using fit()
+        if reference_images is not None:
+            logger.debug("Initialising ``IlluminationCorrection`` using ``reference_images``.")
+            if not isinstance(reference_images, list):
+                raise TypeError("``reference images`` must be a list")
+            else:
+                if self._timelapse is None:
+                    raise ValueError(
+                        "``timelapse`` must be specified when initialising "
+                        + "``IlluminationCorrection`` using ``reference_images``"
+                    )
+                if all(isinstance(image, AICSImage) for image in reference_images):
+                    self.fit(reference_images, **kwargs)
+                elif all(isinstance(image, (str, Path)) for image in reference_images):
+                    images = [AICSImage(image) for image in reference_images]
+                    self.fit(images, **kwargs)
+
+        # 2. Read from a file using load()
+        elif from_file is not None:
+            logger.debug(f"Loading ``IlluminationCorrection`` from {from_file}.")
+            self.file_path = from_file
+            self.load(self._file_path)  # type: ignore
+            if self.timelapse is None:
+                raise ValueError("``timelapse`` is not specified in file {self._file_path}")
+
+        # 3. Initialise empty
+        else:
+            logger.debug("Initialising empty ``IlluminationCorrection`` object.")
+
+    @property
+    def dims(self):
+        return self._dims
+
+    @property
+    def timelapse(self):
+        return self._timelapse
+
+    @property
+    def correctors(self):
+        return self._correctors
+
+    def fit(self, reference_images: List[AICSImage], timelapse: bool = False, **kwargs):
+        try:
+            if not check_uniform_dimension_sizes(reference_images):
+                raise ValueError(
+                    "Check input. One or more of the ``reference_images`` has non-uniform or incorrect dimensionality"
+                )
+        except TypeError:
+            raise TypeError("All reference images should be ``AICSImage``s")
+
+        if not self._timelapse:
+            # use the 'T' axis to concatenate images
+            images = concatenate_images(reference_images, axis="T", order="append")
+        else:
+            # average each timepoint
+            images = average_images(reference_images)
+
+        self._dims = reference_images[0].dims
+        self._correctors = [basicpy.BaSiC(**kwargs) for _ in range(images.dims.C)]
+        for c in range(images.dims.C):
+            self._correctors[c].fit(images.get_image_data("TYX", C=c))
+
+    @property
+    def file_name(self):
+        return self._file_name
+
+    @property
+    def file_path(self):
+        return self._file_path
+
+    @file_path.setter
+    def file_path(self, file_path: Union[str, Path]):
+        if not isinstance(file_path, (str, Path)):
+            raise TypeError("``file_path`` must be a ``str`` or ``pathlib.Path``.")
+
+        p = Path(file_path)
+        if p.suffix == "":
+            raise ValueError("``path`` must be a file, not a directory.")
+        if not p.parent.exists():
+            logger.debug("Creating folder for illumination correction files")
+            p.parent.mkdir()
+
+        self._file_path = p
+        self._file_name = self._file_path.name
+
+    def save(self, path: Union[str, Path]):
+        self.file_path = path
+        if isinstance(self._correctors, list):
+            with open(self._file_path, "wb") as f:  # type: ignore
+                pickle.dump(self, f)
+        else:
+            raise RuntimeError("Cannot save ``IlluminationCorrection`` if correctors are not defined")
+
+    def load(self, path: Union[str, Path]):
+
+        # 1. Load from file
+        with open(path, "rb") as f:  # type: ignore
+            illumination_correction = pickle.load(f)
+
+        # 2. Check attributes of loaded object
+        if str(illumination_correction._file_path) != str(path):
+            logger.warn(
+                f"``file_path =`` {str(illumination_correction._file_path)} in object "
+                + f"stored at {str(path)} does not match. Overwriting new ``file_path``"
             )
-    except TypeError:
-        raise TypeError("All reference images should be ``AICSImage``s")
+            illumination_correction.file_path = path
+        if not isinstance(illumination_correction._correctors, list):
+            raise RuntimeError(f"Object at {path} does not have a list of correctors.")
+        if not all(isinstance(c, basicpy.BaSiC) for c in illumination_correction._correctors):
+            raise RuntimeError(f"Correctors in object at {path} have unrecognised type.")
+        if illumination_correction.dims is None:
+            raise RuntimeError(f"Object at {path} has no ``dims`` attribute.")
+        if illumination_correction.timelapse is None:
+            raise RuntimeError(f"Object at {path} has no ``timelapse`` attribute.")
+        if illumination_correction.dims.C != len(illumination_correction._correctors):
+            raise RuntimeError(
+                f"Object at {path} has ``dims`` = {illumination_correction.dims.C} "
+                + f"but only {len(illumination_correction._correctors)} correctors."
+            )
 
-    if not timelapse:
-        # use the 'T' axis to concatenate images
-        images = concatenate_images(reference_images, axis="T", order="append")
+        # 3. Assign to class instance
+        self._correctors = illumination_correction.correctors
+        self._dims = illumination_correction.dims
+        self._timelapse = illumination_correction.timelapse
+
+    def correct(
+        self, image: Union[AICSImage, np.ndarray, da.core.Array, List[AICSImage], List[np.ndarray], List[da.core.Array]]
+    ):
+        return correct_illumination(image, self)
+
+
+def _correct_illumination(
+    image: Union[AICSImage, np.ndarray, da.core.Array],
+    illumination_correction: IlluminationCorrection,
+) -> Union[AICSImage, np.ndarray]:
+
+    if isinstance(image, np.ndarray):
+        im = AICSImage(image)
+        out_type = "ndarray"
+    elif isinstance(image, da.core.Array):
+        im = AICSImage(image)
+        out_type = "ndarray"
+    elif isinstance(image, AICSImage):
+        im = image
+        out_type = "AICSImage"
     else:
-        # average each timepoint
-        images = average_images(reference_images)
+        out_type = None
+        raise TypeError(f"Unknown input image type {type(image)}")
 
-    illum_corr = [BasicIlluminationCorrection(**kwargs) for _ in range(images.dims.C)]
-    for c in range(images.dims.C):
-        illum_corr[c].fit(images.get_image_data("TYX", C=c))
-
-    return illum_corr
-
-
-def _correct_image(
-    obj: List[BasicIlluminationCorrection],
-    im: AICSImage,
-    timelapse: Union[bool, Literal["multiplicative"], Literal["additive"]] = False,
-):
-    if im.dims.C != len(obj):
-        raise ValueError(f"Incorrect number of correction objects, expected {im.dims.C}.")
+    if not equal_dims(im, illumination_correction, dimensions="TCYX"):
+        raise ValueError(
+            "``IlluminationCorrection`` object has incorrect ``dims``: expected "
+            + f"{im.dims}, found {illumination_correction.dims}."
+        )
     else:
-        if timelapse is False:
+        if illumination_correction.timelapse is False:
             corr = np.stack(
                 [
                     np.stack(
                         [
                             np.stack(
                                 [
-                                    obj[c].transform(
+                                    illumination_correction.correctors[c].transform(
                                         im.get_image_data("YX", C=c, Z=z, T=t),
-                                        timelapse=timelapse,
+                                        timelapse=illumination_correction.timelapse,
                                     )[0]
                                     for z in range(im.dims.Z)
                                 ],
@@ -76,9 +219,9 @@ def _correct_image(
                 [
                     np.stack(
                         [
-                            obj[c].transform(
+                            illumination_correction.correctors[c].transform(
                                 im.get_image_data("TYX", C=c, Z=z),
-                                timelapse=timelapse,
+                                timelapse=illumination_correction.timelapse,
                             )[0]
                             for z in range(im.dims.Z)
                         ],
@@ -89,20 +232,29 @@ def _correct_image(
                 axis=0,
             )
             corr = transpose_to_dims(corr_CZTYX, given_dims="CZTYX", return_dims="TCZYX")
-    return AICSImage(corr, physical_pixel_sizes=im.physical_pixel_sizes, channel_names=im.channel_names)
-
-
-def apply_illumination_correction(
-    correction_objects: BasicIlluminationCorrection,
-    images: Union[AICSImage, List[AICSImage]],
-    timelapse: Union[bool, Literal["multiplicative"], Literal["additive"]] = False,
-) -> Union[AICSImage, List[AICSImage]]:
-
-    if isinstance(images, AICSImage):
-        return _correct_image(correction_objects, images, timelapse=timelapse)
-    elif isinstance(images, list):
-        if not all(isinstance(image, AICSImage) for image in images):
-            raise TypeError("Not all of ``images`` are of class AICSImage")
-        return [_correct_image(correction_objects, image, timelapse=timelapse) for image in images]
+    if out_type == "AICSImage":
+        return AICSImage(corr, physical_pixel_sizes=im.physical_pixel_sizes, channel_names=im.channel_names)
     else:
-        raise TypeError("``images`` should be an AICSImage or list of AICSImages")
+        return corr
+
+
+def correct_illumination(
+    images: Union[AICSImage, np.ndarray, da.core.Array, List[AICSImage], List[np.ndarray], List[da.core.Array]],
+    illumination_correction: IlluminationCorrection,
+) -> Union[AICSImage, np.ndarray, List[AICSImage], List[np.ndarray]]:
+
+    # individual input -> individual output
+    if isinstance(images, (AICSImage, np.ndarray, da.core.Array)):
+        res = _correct_illumination(images, illumination_correction)
+    # list input -> list output
+    elif isinstance(images, list):
+        if all(isinstance(image, AICSImage) for image in images):
+            res = [_correct_illumination(image, illumination_correction) for image in images]
+        elif all(isinstance(image, np.ndarray) for image in images):
+            res = [_correct_illumination(image, illumination_correction) for image in images]
+        elif all(isinstance(image, da.core.Array) for image in images):
+            res = [_correct_illumination(image, illumination_correction) for image in images]
+    else:
+        raise TypeError("``images`` is a non-uniform list")
+
+    return res
