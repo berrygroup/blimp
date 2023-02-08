c7cf4e2614f5bc82e753cc93e90f88d00a119940
scottberry <scottdberry@gmail.com>
2023-02-08 21:21:48 +1100

    example_data update paths

--- notebooks/ExampleData_constants.py
+++ notebooks/ExampleData_constants.py
@@ -4,7 +4,5 @@ import os
 from blimp.constants import blimp_config
 
 # --- dataset specific constants ---
-DATA_DIR = blimp_config.BASE_DATA_DIR
-DATASET_DIR = os.path.join(DATA_DIR, "datasets")
-RESOURCES_DIR = os.path.join(DATA_DIR, "resources")
-RESULTS_DIR = os.path.join(blimp_config.EXPERIMENT_DIR, "example_results")
+DATA_DIR = os.path.join(blimp_config.BASE_DATA_DIR, "raw", "_data")
+EXPERIMENT_DIR = blimp_config.EXPERIMENT_DIR
