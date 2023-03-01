059b207475ad79e4ebb1b8f28a7449902b049977
scottberry <scottdberry@gmail.com>
2023-03-01 23:17:56 +1100

    increased memory and time for nd2 convert template

--- blimp/templates/convert_nd2_pbs.sh
+++ blimp/templates/convert_nd2_pbs.sh
@@ -3,8 +3,8 @@
 ### Splits nd2 files into standard OME-TIFF format
 
 #PBS -N ConvertND2
-#PBS -l select=1:ncpus=1:mem=8gb
-#PBS -l walltime=02:00:00
+#PBS -l select=1:ncpus=1:mem=32gb
+#PBS -l walltime=04:00:00
 #PBS -o {LOG_DIR}/
 #PBS -e {LOG_DIR}/
 #PBS -M {USER_EMAIL}
