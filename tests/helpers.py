05d4702cd765fad0b70ff45c19bd4dacd169a033
Scott Berry <scottdberry@gmail.com>
2024-10-29 19:38:10 +1100

    added synthetic data to test helper function

--- tests/helpers.py
+++ tests/helpers.py
@@ -48,5 +48,17 @@ def _load_test_data(dataset: str) -> Union[List[AICSImage], None]:
         bf01 = AICSImage(dataset_path / "221103_brightfield_488_568_647_1.nd2")
         bf02 = AICSImage(dataset_path / "221103_brightfield_488_568_647_2.nd2")
         return [bf01, bf02]
+    elif dataset == "synthetic_2D":
+        dataset_path = Path(testdata_config.DATASET_DIR) / "synthetic_images"
+        logger.info(f"Loading registration test images from {dataset_path}")
+        syn_intensity01 = AICSImage(dataset_path / "synthetic_intensity_image_TYX.tiff")
+        syn_label02 = AICSImage(dataset_path / "synthetic_label_image_TYX.tiff")
+        return [syn_intensity01, syn_label02]
+    elif dataset == "synthetic_3D":
+        dataset_path = Path(testdata_config.DATASET_DIR) / "synthetic_images"
+        logger.info(f"Loading registration test images from {dataset_path}")
+        syn_intensity01 = AICSImage(dataset_path / "synthetic_intensity_image_TZYX.tiff")
+        syn_label02 = AICSImage(dataset_path / "synthetic_label_image_TZYX.tiff")
+        return [syn_intensity01, syn_label02]
 
     return None
